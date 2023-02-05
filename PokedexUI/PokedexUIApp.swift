33dc6595fb97d271d674cc792446edd4eccd4fad
gustonecrush <codeboxezz@gmail.com>
2023-02-05 07:32:32 +0700

    initialized project

---
+++ PokedexUI/PokedexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexUIApp.swift
+//  PokedexUI
+//
+//  Created by A Farhan Agustiansyah on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
